a791c0d1f4d388e9b82c8a85fef2a2f69d20d590
Robert Greenshields <bob@thegreenshields.com>
2011-05-26 09:29:12 +0100

    refactor BackupEnv to use module BaseEnv

--- lib/backup_env.rb
+++ lib/backup_env.rb
@@ -1,73 +1,39 @@
+require 'base_env'
+
 class BackupEnv
-	def initialize
-		@envs = {}
-		@passkeys = %w(default, backup, restore)
-		@pass = {}
-	end
 
-	def include?(key)
-		@envs.key? key
-	end
+	include BaseEnv
 
-	def pass_include?(key)
-		@pass.key? key
-	end
-
-	def [](key)
-		@envs[key]
-	end
-
-	def pass_val(name)
-		@pass[name]
-	end
-
-	def add_item(env, val)
-		unless env.kind_of? String
-			raise ArgumentError, "The value #{env} is not a valid environment var name"
-		end
-		val = "" if val.nil?
-		unless val.kind_of? String
-			raise(ArgumentError, "The env variable #{env} cannot be set with the " +
-				"value #{val}, must be a string")
-		end
-		key = env.upcase
-		@envs[key] = val unless include?(key)
-	end
-
-	def add_pass(name, val)
+	def check_add_item_args(name, val)
 		unless name.kind_of? String
-			raise ArgumentError, "The value #{name} is not a valid passphrase name"
+			raise ArgumentError, "The value #{name} is not a valid environment var name"
 		end
-		val = "" if val.nil?
+		if name == ""
+			raise ArgumentError, "An environment var name cannot be an empty string"
+		end
 		unless val.kind_of? String
-			raise(ArgumentError, "The passphrase #{name} cannot be set with the " +
-				"value #{val}, must be a string")
+			raise(ArgumentError, "The environment variable #{name} cannot be set with " +
+				"the value #{val}, must be a string")
 		end
-
-		
 	end
 
-
-	def fill(envhash)
-		unless envhash.kind_of? Hash
-			raise ArgumentError, "A BackupEnv must be filled with a hash"
+	def check_fill_arg(fillval)
+		unless fillval.kind_of? Hash
+			raise ArgumentError, "Fill must be passed a hash to fill from not a #{fillval.class}"
 		end
-		envhash.each { |k,v| add_item(k, v) }
+		fillval
 	end
 
-	def set_env_value(name, value)
-#		ENV[name] = value
+	def set_env_val(name, val)
+#		Env[name] = val
 	end
 
 	def set
-		@envs.each { |k, v| set_env_val(k, v) }
+		envs.each { |k, v| set_env_val(k, v) }
 	end
 
 	def clear
-		@envs.each { |k, v| set_env_val(k, "") }
+		envs.each { |k, v| set_env_val(k, "") }
 	end
-
-
-
 
 end
--- spec/backup_env_spec.rb
+++ spec/backup_env_spec.rb
@@ -5,79 +5,40 @@ describe "BackupEnv" do
 		@env = BackupEnv.new
 	end
 
-	describe "#add_item" do
-		it "should add a pair to the hash" do
-			key = "FIRST"
-			val = "this"
-			@env.add_item(key, val)
-			@env.should include key
-			@env[key].should == val
-		end
-
-		it "should capitalise the environment variable name" do
-			key = "first"
-			capkey = "FIRST"
-			val = "this"
-			@env.add_item(key, val)
-			@env.should include capkey
-			@env[capkey].should == val
-		end
-
-		it "should not add to an env var a second time" do
-			key = "FIRST"
-			val = "first"
-			val2 = "second"
-			@env.add_item(key, val)
-			@env.add_item(key, val2)
-			@env.should include key
-			@env[key].should == val
-		end
-
+	describe "#check_add_item_args" do
 		it "should raise an error if an env var name is not a string" do
 			key = 12
 			val = "first"
-			lambda { @env.add_item(key, val) }.should raise_error(
+			lambda { @env.check_add_item_args(key, val) }.should raise_error(
 				ArgumentError, "The value 12 is not a valid environment var name")
 		end
 
-		it "should convert a nil value into an empty string value" do
-			key = "FIRST"
-			val = nil
-			@env.add_item(key, val)
-			@env.should include key
-			@env[key].should == ""
+		it "should raise an error if an env var name is the empty string" do
+			key = ""
+			val = "first"
+			lambda { @env.check_add_item_args(key, val) }.should raise_error(
+				ArgumentError, "An environment var name cannot be an empty string")
 		end
 
-		it "should raise an error if an env var value is not a string" do
+		it "should raise an error if an environment var value is not a string" do
 			key = "FIRST"
 			val = 12
-			lambda { @env.add_item(key, val) }.should raise_error(
-				ArgumentError, "The env variable #{key} cannot be set with the value #{val}, must be a string")
+			lambda { @env.check_add_item_args(key, val) }.should raise_error(
+				ArgumentError, "The environment variable #{key} cannot be set with " +
+					"the value #{val}, must be a string")
 		end
-
 	end
 
-		describe "#fill" do
+		describe "#check_fill_arg" do
 			it "should raise an error if not passed an hash" do
-				lambda { @env.fill "this" }.should raise_error(
-					ArgumentError)
+				lambda { @env.check_fill_arg "this" }.should raise_error(
+					ArgumentError, "Fill must be passed a hash to fill from not a String")
 			end
 
-			it "should call add_item for each element" do
-				@env.stub(:add_item)
-				@env.should_receive(:add_item).exactly(3).times
-				h = {"one" => "one", "two" => "two", "three" => "three"}
-				@env.fill h
+			it "should return a hash" do
+				@env.check_fill_arg({}).should be_a Hash
 			end
-			
-			it "should call add_item with the name and value" do
-				@env.stub(:add_item)
-				name = "first"
-				val = "firstval"
-				@env.should_receive(:add_item).with(name, val)
-				h = {name => val}
-				@env.fill h
-			end
+
 		end
 
 		describe "#set" do
