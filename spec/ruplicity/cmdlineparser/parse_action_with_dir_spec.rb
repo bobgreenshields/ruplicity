b8a98d382f5f0070a5a055b2b4d2485d713f2d99
Bob Greenshields <bob@thegreenshields.com>
2013-11-22 21:36:38 +0000

    correct method call in shared examples

--- spec/ruplicity/cmdlineparser/parse_action_with_dir_spec.rb
+++ spec/ruplicity/cmdlineparser/parse_action_with_dir_spec.rb
@@ -4,9 +4,9 @@ require_relative '../../../lib/ruplicity/cmdlineparser/parse_action_with_dir'
 describe ParseActionWithDir do
 	it_behaves_like "an action parser"
 
-	def parser
-		ParseActionWithDir.new
-	end
+#	def parser
+#		ParseActionWithDir.new
+#	end
 
 
 
