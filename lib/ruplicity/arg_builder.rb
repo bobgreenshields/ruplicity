66e397ef77d1104463e09108003c41a784ba50dd
Bob Greenshields <bob@thegreenshields.com>
2013-11-24 21:11:41 +0000

    finish options from keys

--- lib/ruplicity/arg_builder.rb
+++ lib/ruplicity/arg_builder.rb
@@ -14,11 +14,27 @@ module Ruplicity
 			opt[2, opt.length-2].gsub('-', '_').to_sym
 		end
 
+		def opt_from_key(key)
+			"--" + key.to_s.gsub('_', '-')
+		end
+
 		def argless_options_from_keys(args, opts)
 			key_options = %w(--dry-run  --force  --s3-use-new-style  --s3-european-buckets)
 			key_options.select do | opt |
 				!opts.include?(opt) and args.has_key?(key_from_opt(opt))
-			end
+			end + opts
+		end
+
+		def options_with_args_from_keys(args, opts)
+			keys = [:name, :encrypt_key, :full_if_older_than, :sign_key, :verbosity]
+			keys.inject([]) do | new_opts, key |
+				unless args.has_key?(key)
+					new_opts
+				else
+					opts.delete_if { | opt | /#{opt_from_key(key)}/.match(opt) }
+					new_opts << "#{opt_from_key(key)} #{args[key]}"
+				end
+			end + opts
 		end
 	end
 end
--- spec/ruplicity/arg_builder_spec.rb
+++ spec/ruplicity/arg_builder_spec.rb
@@ -43,17 +43,40 @@ describe ArgBuilder do
 			@opts = ["--this this-val", "--dry-run"]
 		end
 
-		it "should return an option for a key if not present" do
+		it "should prepend an option for a key if not present" do
 			args = {name: "new run", dry_run: nil, force: "this", s3_use_new_style: "that",
 				s3_european_buckets: nil}
 			opts = ["--this this-val"]
-			expected = %w(--dry-run --force --s3-use-new-style --s3-european-buckets)
+			expected = %w(--dry-run --force --s3-use-new-style --s3-european-buckets) + opts
 			argb.argless_options_from_keys(args, opts).should eql(expected)
 		end
 
 		it "should ignore a key if the option is already present" do
-			argb.argless_options_from_keys(@args, @opts).should_not include("--dry-run")
-			argb.argless_options_from_keys(@args, @opts).should include("--force")
+			argb.argless_options_from_keys(@args, @opts)[2].should eql("--dry-run")
+			argb.argless_options_from_keys(@args, @opts)[0].should eql("--force")
+		end
+	end
+
+	describe "#options_with_args_from_keys" do
+		let (:argb) { ArgBuilder.new }
+		before :each do
+			@args = {name: "thisname", verbosity: "8", full_if_older_than: "30days",
+				encrypt_key: "ABB12345", sign_key: "ABCDEF12", dry_run: nil}
+			@opts = ["--this this-val"]
+		end
+
+		it "should return an option for a key if not present" do
+			expected = ["--name thisname", "--encrypt-key ABB12345", "--full-if-older-than 30days", 
+							  "--sign-key ABCDEF12", "--verbosity 8"] + @opts
+			argb.options_with_args_from_keys(@args, @opts).should eql(expected)
+		end
+
+		it "should delete an option if present and prepend value from key" do
+			opts = @opts + ["--sign-key 12345678", "--verbosity 3"]
+			expected = ["--name thisname",  "--encrypt-key ABB12345", "--full-if-older-than 30days",
+							  "--sign-key ABCDEF12", "--verbosity 8"] + @opts
+			argb.options_with_args_from_keys(@args, opts).should eql(expected)
+
 		end
 
 	end
