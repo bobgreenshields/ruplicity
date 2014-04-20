066f4fb3e38f67ce8cc7c03e8ab3d3a8c5cb387b
Robert Greenshields <bob@thegreenshields.com>
2014-04-20 20:28:45 +0100

    rename shared examples for builder

--- spec/support/shared_examples_for_cmd_parser.rb
+++ spec/support/shared_examples_for_cmd_parser.rb
@@ -1,11 +1,11 @@
 require_relative './shared_examples_for_set_errors'
 
-shared_examples "a cmd_parser" do
-	let(:parser) { described_class.new({}) }
+shared_examples "a cmd_builder" do
+	let(:builder) { described_class.new({}) }
 
 	it_behaves_like "it can set_errors"
 
 	describe "#cmd_arr" do
-		specify { parser.should respond_to(:cmd_arr) }
+		specify { builder.should respond_to(:cmd_arr) }
 	end
 end
