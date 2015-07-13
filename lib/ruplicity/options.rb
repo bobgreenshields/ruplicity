c63a64420612c41d6200b60fa689c3ade84c0f80
Bob Greenshields <bob@thegreenshields.com>
2015-07-13 20:40:13 +0100

    basic functionality done

--- lib/ruplicity/options.rb
+++ lib/ruplicity/options.rb
@@ -1,7 +1,25 @@
 module Ruplicity
 	class Options
+		attr_reader :options
+
 		def initialize(option_array)
-			
+			@options = Array(option_array).map &:strip
+			@key_lookup = Hash.new { | hash, key | hash[key] = [] }
+			load_key_lookup
+		end
+
+		def load_key_lookup
+			@options.each_with_index do | option, index |
+				@key_lookup[key_from_option(option)] << index
+			end
+		end
+
+		def key_indices(key)
+			if @key_lookup.has_key?(key)
+				@key_lookup[key]
+			else
+				[]
+			end
 		end
 
 		def key_from_option(option)
@@ -11,7 +29,16 @@ module Ruplicity
 			else
 				raise ArgumentError, "option #{option} does not have a good format"
 			end
-			
+		end
+
+		def add(option)
+			index_arr = @key_lookup[key_from_option(option)]
+			if index_arr.empty?
+				@options << option
+				index_arr << (@options.length - 1)
+			else
+				@options[index_arr.last] = option
+			end
 		end
 		
 	end
--- spec/ruplicity/options_spec.rb
+++ spec/ruplicity/options_spec.rb
@@ -30,6 +30,59 @@ describe Options do
 				to raise_error(ArgumentError, "option force does not have a good format")
 		end
 
+		describe "#options" do
+			it "should always return an array" do
+				expect(Options.new("--force").options).to be_an(Array)
+			end
+
+			it "should strip leading and trailing whitespaces" do
+				expect(Options.new(["   --force", "  --sign-key A1234  "]).options).
+					to eq(["--force", "--sign-key A1234"])
+			end
+		end
+
+		describe "key_indices" do
+			let(:opt) { Options.new(["--force", "--exclude this", "--sign-key", "--exclude that"]) }
+			
+			it "should always return an array" do
+				expect(opt.key_indices(:force)).to be_an(Array)
+			end
+			
+			it "return an array with the index of the option" do
+				expect(opt.key_indices(:sign_key)).to eq([2])
+			end
+			
+			it "return an array with all indices for multiple same options" do
+				expect(opt.key_indices(:exclude)).to eq([1,3])
+			end
+		end
+
+    describe "#add" do
+			before(:example) do
+				@opt = Options.new(["--force", "--exclude this", "--exclude that", "--sign-key"])
+			end
+
+			it "should add a new option to the end of the options array" do
+				@opt.add("--dry-run")
+				expect(@opt.options.last).to eq("--dry-run")
+			end
+
+			it "should locate the index of a new option" do
+				@opt.add("--dry_run")
+				expect(@opt.key_indices(:dry_run)).to eq([4])
+			end
+
+			it "should replace the last occurence of a duplicate option" do
+				@opt.add("--exclude another")
+				expect(@opt.options[2]).to eq("--exclude another")
+			end
+
+			it "should change the indices of a duplicate option" do
+				@opt.add("--exclude another")
+				expect(@opt.key_indices(:exclude)).to eq([1,2])
+			end
+    end
+
 	end
 	
 end
