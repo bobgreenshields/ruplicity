c2ce4542c342120a263f77386fcff00fa98298c9
Bob Greenshields <bob@thegreenshields.com>
2015-07-16 11:49:45 +0100

    amend to use correct syntax for class variables

--- lib/ruplicity/cmd.rb
+++ lib/ruplicity/cmd.rb
@@ -1,24 +1,29 @@
 module Ruplicity
 	class Cmd
-		@@cmd_items_to_build = []
 
-		def self.add_cmd_item_to_build(cmd_item_class, name)
-			@@cmd_items_to_build << Cmd_Item_Builder.new(cmd_item_class, name)
+		class << self
+			def cmd_items_to_build
+				@cmd_items_to_build ||= []
+			end
+			
+			def add_cmd_item_to_build(cmd_item_class, name)
+				cmd_items_to_build << CmdItemBuilder.new(cmditemclass, name)
+			end
 		end
 
 		def initialize
-			@cmd_items = @@cmd_items_to_build.map &:build
+			@cmd_items = self.class.cmd_items_to_build.map &:build
 		end
 	end
 
-	class Cmd_Item_Builder
+	class CmdItemBuilder
 		def initialize(cmd_item_class, name)
 			@cmd_item_class = cmd_item_class
 			@name = name
 		end
 
 		def build
-			@cmd_item_class.initialize(name)
+			@cmd_item_class.initialize(@name)
 		end
 	end
 end
