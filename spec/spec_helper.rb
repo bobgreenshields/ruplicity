e9d6360ad3239f7aa22779b8bddf8498ff4a4f40
Bob Greenshields <bob@thegreenshields.com>
2016-08-01 19:01:53 +0100

    add load path for lib directory

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
-#require 'ruplicity'
-#require 'testlogger'
+lib_dir = File.join(File.dirname(__FILE__), '../lib')
+full_lib_dir = File.expand_path(lib_dir)
+$LOAD_PATH.unshift(full_lib_dir) unless
+	$LOAD_PATH.include?(lib_dir) || $LOAD_PATH.include?(full_lib_dir)
 
 Dir["./spec/support/**/*.rb"].each { |f| require f }
 
 include Ruplicity
+
